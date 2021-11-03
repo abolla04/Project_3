8db0fa41e958e958936d67cf290e60fb83fa07f3
mckayav3 <84061524+mckayav3@users.noreply.github.com>
2021-11-02 20:49:43 -0700

    Update Time_Sensitive.sol

--- _Time_Sensitive/Time_Sensitive.sol
+++ _Time_Sensitive/Time_Sensitive.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
-import "./sonatsonos.sol";
+import "./SonatSonos.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
