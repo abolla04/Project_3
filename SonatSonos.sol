30247bc55f9f3468981ca6145b2563112069fd54
mckayav3 <84061524+mckayav3@users.noreply.github.com>
2021-11-06 20:19:46 -0700

    added comments

--- SonatSonos.sol
+++ SonatSonos.sol
@@ -9,7 +9,6 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
 // Create a constructor for the Sonat Sonos contract and have the contract inherit the libraries that you imported from OpenZeppelin.
-
 contract SonatSonos is ERC20, ERC20Detailed, ERC20Mintable {
     constructor(
         string memory name,
--- _Time_Sensitive/Time_Sensitive.sol
+++ _Time_Sensitive/Time_Sensitive.sol
@@ -1,21 +1,37 @@
 pragma solidity ^0.5.0;
 
+/*
+Import the following contracts from the OpenZeppelin library and our SonatSonos:
+
+* `Crowdsale`
+* `MintedCrowdsale`
+* `CappedCrowdsale`
+* `TimedCrowdsale`
+
+*/
+
 import "./SonatSonos.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
 
-
+/*
+Have Time Sensitive contract inherit the following from OpenZeppelin:
+* `Crowdsale`
+* `MintedCrowdsale`
+* `CappedCrowdsale`
+* `TimedCrowdsale`
+*/
 contract TimeSensitiveCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale{
-
-	//minimum investor Contribution - 2 ether
-	//minimum investor Contribution - 50 ether
+	// sets a min and max in ether 
+	//minimum - 2 ether
+	//maximum - 50 ether
 	uint256 public investorMinCap = 2000000000000000000;
 	uint256 public investorHardCap = 50000000000000000000;
 
 	mapping(address => uint256) public contributions;
-
+	// Provide parameters for all the features of the crowdsale
 	constructor(uint256 _rate,
 	  address payable _wallet,
 	  ERC20 _token,
@@ -28,7 +44,7 @@ contract TimeSensitiveCrowdsale is Crowdsale, MintedCrowdsale, CappedCrowdsale, 
 	public{
 	}
 
-
+//Define a `preValidatePurchase` function
   function _preValidatePurchase(
     address payable _beneficiary,
     uint256 _weiAmount
